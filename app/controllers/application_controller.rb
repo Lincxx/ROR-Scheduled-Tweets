38fc74b7e1ea617b7c64a24d600ce74ad99614dc
Lincxx <bossco28@gmail.com>
2021-11-19 07:19:40 -0500

    Created a current model, Update application_contoller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    # all controllers inherit from here
+
+    before_action :set_current_user
+    
+    def set_current_user
+        if session[:user_id]  
+            Current.user = User.find_by(id: session[:user_id])
+        end
+    end
 end
---
+++ app/models/current.rb
@@ -0,0 +1,7 @@
+class Current < ActiveSupport::CurrentAttributes
+    # Here we can assign items such as user, timezone, account thy are on, those types of detials, that pretain to the current request. So when someone opens the website. They may not be logged in, so current user will be nil, so the inverse for someone who is already logged in, they current user will be them
+
+    #this does not have to be used "GoRails", but is easy to use
+    attribute :user
+
+end
