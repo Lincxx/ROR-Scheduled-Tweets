a0ded7257a455c040b6ac79150bc4944737f8bc4
Lincxx <bossco28@gmail.com>
2021-11-20 12:56:44 -0500

    Added edit, update password, build a view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,4 +17,26 @@ class PasswordResetsController < ApplicationController
         #we will send this whether or not the user was found in the db.
         redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password?"
     end
+
+    def edit 
+        # find_signed! will throw an error
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Your token has expired. Please try again."
+        #binding.irb
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in"
+        else
+            render :edit #render the edit page
+        end
+    end
+
+    private
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
 end
