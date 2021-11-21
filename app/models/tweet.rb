88743eaf1158b8df8e10e8108564d3fa873eeef7
Lincxx <bossco28@gmail.com>
2021-11-21 08:16:30 -0500

    Added in some validation on the twetts

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,14 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: {minimum: 1, maximum: 280}
+  validates :publish_at, presence: true
+
+  after_initialize do 
+    #||=
+    # if a is undefined or falsey, then evaluate b and set a to the result.
+    #Otherwise (if a is defined and evaluates to truthy), then b is not evaluated, and no assignment takes place.
+    self.publish_at ||= 24.hour.from_now
+  end
 end
